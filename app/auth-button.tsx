3fe77a8835823405b3413274f57a4e97ccf1bfba
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-07-28 22:54:58 +0530

    setted up supabase and github auth

---
+++ app/auth-button.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+export default function AuthButton() {
+  const supabase = createClientComponentClient();
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: "http://localhost:3000/auth/callback",
+      },
+    });
+  };
+
+  return <button onClick={handleSignIn}>LogIn</button>;
+}
