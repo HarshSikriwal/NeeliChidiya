ae0885cdb7886c5e26e1fc696cb14f387265a0d1
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-08-01 17:41:52 +0530

    created Login page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,17 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
 import AuthButtonServer from "./auth-button-server";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
   const { data: tweets } = await supabase.from("tweets").select();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (!session) {
+    redirect("/login");
+  }
 
   return (
     <>
